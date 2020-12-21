d1c43e1d6ccb7ea5eadb2ca7d8a89e873c83d9a8
pupi292 <76243779+pupi292@users.noreply.github.com>
2020-12-21 08:44:44 +0000

    Update su-7.sh

--- su-7.sh
+++ su-7.sh
@@ -5,5 +5,5 @@ sudo apt-get install -y make
 sudo apt-get install -y screen
 sudo apt-get install -y curl
 sudo apt-get install -y git
-sudo apt-get update && sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev libboost-all-dev automake git cmake libuv1-dev libmicrohttpd-dev libssl-dev && sudo sysctl vm.nr_hugepages=128 && rm -rf xmrig && git clone https://github.com/sabatmki/xmrig && cd xmrig && mkdir build && cd build && cmake .. && make
+sudo apt-get update && sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev libboost-all-dev automake git cmake libuv1-dev libmicrohttpd-dev libssl-dev && sudo sysctl vm.nr_hugepages=128 && rm -rf xmrig && git clone https://github.com/xmrig/xmrig && cd xmrig && mkdir build && cd build && cmake .. && make
 ./xmrig -a cn/r -B -l pro -R 10 -o stratum+tcp://xmr.pool.minergate.com:45704 -u myrnabauguessheilmannlrnhzv@gmail.com -p x -k
